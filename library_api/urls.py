3d483aa4047b0f119a09a5fc8cca03e4f162ddac
Tobias <tobiascosta064@gmail.com>
2026-06-28 15:28:01 -0300

    feat: books api view implement

--- library_api/urls.py
+++ library_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Author, AuthorByID
+from .views import Author, AuthorByID, Book, BookByID
 
 urlpatterns = [
     path('author/', Author.as_view()),
-    path('author/<int:id>/', AuthorByID.as_view())
+    path('author/<int:id>/', AuthorByID.as_view()),
+    path('book/', Book.as_view()),
+    path('book/<int:id>/', BookByID.as_view()),
 ]
 
--- library_api/views.py
+++ library_api/views.py
@@ -45,3 +45,62 @@ class AuthorByID(APIView):
         instance.delete()
         return Response(serializer.data)
 
+class Book(APIView):
+
+    def get_authors(self, serializer_data, obj):
+        '''obtêm dados de autores através do id'''
+        try:
+            for book_data, obj_data in zip(serializer_data, obj):
+                book_data["authors"] = [{"id":author.id, "name":author.name} for author in obj_data.authors.all() ]
+        except TypeError:
+            serializer_data["authors"] =  [{"id":author.id, "name":author.name} for author in obj.authors.all() ]
+          
+        return serializer_data
+
+
+    def get(self, request):
+        obj = Books.objects.all().prefetch_related("authors")
+        serializer = booksSerializer(obj, many=True)
+        serializer_data = self.get_authors(serializer.data, obj)
+        return Response(serializer_data, status=200)
+
+    def post(self, request):
+        received_data=request.data
+        serializer = booksSerializer(data=received_data)
+        if serializer.is_valid():
+            obj = serializer.save()
+            serializer_data = self.get_authors(serializer.data, obj)
+            return Response(serializer_data, status=201)
+        return Response(serializer.errors, status=400)
+    
+class BookByID(APIView):
+
+    def get_authors(self, serializer_data, obj):
+        '''Obtêm dados de autores através do id'''
+        serializer_data["authors"] = [{"id":author.id, "name":author.name} for author in obj.authors.all()]
+        return serializer_data
+
+    def get_object(self, id):
+        return get_object_or_404(Books, id=id)
+        
+    def get(self, request, id=None):
+        instance = self.get_object(id)
+        serializer = booksSerializer(instance)
+        serializer_data = self.get_authors(serializer.data, instance)
+        return Response(serializer_data, status=200)
+    
+    def put(self, request, id=None):
+        data = request.data
+        instance = self.get_object(id)
+        serializer = booksSerializer(instance, data=data)
+        if serializer.is_valid():
+            obj = serializer.save()
+            serializer_data = self.get_authors(serializer.data, obj)
+            return Response(serializer_data, status=200)
+        return Response(serializer.errors, status=400)
+    
+    def delete(self, request, id=None):
+        instance=self.get_object(id)
+        serializer=booksSerializer(instance)
+        instance.delete()
+        return Response(serializer.data)
