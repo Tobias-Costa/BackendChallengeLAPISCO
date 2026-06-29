c64d404cd8cb19f757482e29e2afa9b8de2414a3
Tobias <tobiascosta064@gmail.com>
2026-06-28 23:40:42 -0300

    fix: url singular error

--- library_api/urls.py
+++ library_api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import Author, AuthorByID, Book, BookByID
 
 urlpatterns = [
-    path('author/', Author.as_view(), name="author_list"),
-    path('author/<int:id>/', AuthorByID.as_view(),  name="author_detail"),
-    path('book/', Book.as_view(),  name="book_list"),
-    path('book/<int:id>/', BookByID.as_view(),  name="book_detail"),
+    path('authors/', Author.as_view(), name="author_list"),
+    path('authors/<int:id>/', AuthorByID.as_view(),  name="author_detail"),
+    path('books/', Book.as_view(),  name="book_list"),
+    path('books/<int:id>/', BookByID.as_view(),  name="book_detail"),
 ]
 
